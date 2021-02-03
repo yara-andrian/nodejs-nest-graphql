f89f4234e9510c03304c1df17aa18762823143c3
yara-andrian <andrian.kanta@yara.com>
2021-02-03 23:45:20 +0800

    fix issue

--- src/database/migrations/1612365459826-usertable.ts
+++ src/database/migrations/1612365459826-usertable.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class usertable1612365459826 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(500) NOT NULL, "email" text NOT NULL, "phone" character varying(15) NOT NULL, "address" text NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now()`);
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(500) NOT NULL, "email" text NOT NULL, "phone" character varying(15) NOT NULL, "address" text NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now())`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
